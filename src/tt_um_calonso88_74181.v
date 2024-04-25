17c1cad6bc353327df0b3ce2f01476a410459196
Caio Alonso da Costa <caioalonso@gmail.com>
2024-04-25 22:54:18 +0200

    Update tt_um_calonso88_74181.v

--- src/tt_um_calonso88_74181.v
+++ src/tt_um_calonso88_74181.v
@@ -20,10 +20,12 @@ module tt_um_calonso88_74181 (
   wire [3:0] a, b, s;
   wire cn, m;
   wire [3:0] f;
-  wire cn4, equal, p, g;
+  wire cn4, equal;
+  wire [7:0] decod;
 
-  // Assign IOs as input
-  assign uio_oe = '0;
+  // Assign IOs [5:0] as input and IOs[7:6] as outputs
+  assign uio_oe[5:0] = '0;
+  assign uio_oe[7:6] = '1;
   
   // Input ports
   assign a = ui_in[3:0];
@@ -33,15 +35,14 @@ module tt_um_calonso88_74181 (
   assign cn = uio_in[5];
 
   // Output ports
-  assign uo_out[3:0] = f;
-  assign uo_out[4] = cn4;
-  assign uo_out[5] = equal;
-  assign uo_out[6] = p;
-  assign uo_out[7] = g;
+  assign uo_out[7:0] = decod;
+  assign uio_out[6] = cn4;
+  assign uio_out[7] = equal;
   // All output pins must be assigned. If not used, assign to 0.
-  assign uio_out = '0;
+  assign uio_out[5:0] = '0;
 
-  // Instance
-  alu_74181 alu_74181_inst (.a(a), .b(b), .cn(cn), .s(s), .m(m), .f(f), .cn4(cn4), .equal(equal), .p(p), .g(g));
+  // Instances
+  alu_74181 alu_74181_inst (.a(a), .b(b), .cn(cn), .s(s), .m(m), .f(f), .cn4(cn4), .equal(equal), .p(), .g());
+  bin_to_7seg_decoder bin_to_7seg_decoder_inst (.bin(f), .a(decod[0]), .b(decod[1]), .c(decod[2]), .d(decod[3]), .e(decod[4]), .f(decod[5]), .g(decod[6]), .dp(decod[7]));
 
 endmodule
