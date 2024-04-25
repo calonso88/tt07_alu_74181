43cabcdee0d6969511494f7ff1f97b352623153b
Caio Alonso da Costa <caioalonso@gmail.com>
2024-04-25 21:21:48 +0200

    Update alu_74181_logic.sv

--- src/alu_74181_logic.sv
+++ src/alu_74181_logic.sv
@@ -3,18 +3,8 @@ module alu_74181_logic (a, b, s, f);
   input logic [3:0] a, b, s;
   output logic [3:0] f;
   
-  // Function types
-  typedef enum logic [3:0] {
-    FUNC_00, FUNC_01, FUNC_02, FUNC_03, FUNC_04, FUNC_05, FUNC_06, FUNC_07, FUNC_08, FUNC_09, FUNC_10, FUNC_11, FUNC_12, FUNC_13, FUNC_14, FUNC_15
-  } func_type;
-
-  // Auxiliar variables
-  func_type func; 
+  // Auxiliar variable
   logic [3:0] temp;
-
-  // Function selector
-  //assign func = func_type'(s);
-  assign func = s;
 
   // Outputs
   assign f = temp;
@@ -22,72 +12,76 @@ module alu_74181_logic (a, b, s, f);
   // Implement logical operation based on selection
   always_comb begin
 
-    case (func)
+    case (s)
       
-      FUNC_00 : begin
+      4'b0000: begin
         temp = ~a;
       end
 
-      FUNC_01 : begin
+      4'b0001 : begin
         temp = ~(a | b);
       end
 
-      FUNC_02 : begin
+      4'b0010 : begin
         temp = (~a) & b;
       end
 
-      FUNC_03 : begin
+      4'b0011 : begin
         temp = '0;
       end
       
-      FUNC_04 : begin
+      4'b0100 : begin
         temp = ~(a & b);
       end
 
-      FUNC_05 : begin
+      4'b0101 : begin
         temp = (~b);
       end
 
-      FUNC_06 : begin
+      4'b0110 : begin
         temp = a ^ b;
       end
 
-      FUNC_07 : begin
+      4'b0111 : begin
         temp = a & (~b);
       end
 
-      FUNC_08 : begin
+      4'b1000 : begin
         temp = (~a) | b;
       end
 
-      FUNC_09 : begin
+      4'b1001 : begin
         temp = ~(a ^ b);
       end
 
-      FUNC_10 : begin
+      4'b1010 : begin
         temp = b;
       end
 
-      FUNC_11 : begin
+      4'b1011 : begin
         temp = a & b;
       end
 
-      FUNC_12 : begin
+      4'b1100 : begin
         temp = '1;
       end
 
-      FUNC_13 : begin
+      4'b1101 : begin
         temp = a | (~b);
       end
 
-      FUNC_14 : begin
+      4'b1110 : begin
         temp = a | b;
       end
 
-      FUNC_15 : begin
+      4'b1111 : begin
         temp = a;
       end
 
+      default : begin
+        temp = '0;
+      end
+      
     endcase
 
   end
