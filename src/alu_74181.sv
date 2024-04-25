4fa7cc7d20bd0a146b650f939fc431d91365a96a
Caio Alonso da Costa <caioalonso@gmail.com>
2024-04-25 21:53:45 +0200

    Update alu_74181.sv

--- src/alu_74181.sv
+++ src/alu_74181.sv
@@ -7,14 +7,16 @@ module alu_74181 (a, b, cn, s, m, f, cn4, equal, p, g);
 
   // Auxiliar signals
   logic [3:0] f_logic;
+  logic [3:0] f_arith;
 
   // Instance for logic operations
   alu_74181_logic alu_74181_logic_inst (.a(a), .b(b), .s(s), .f(f_logic));
+  // Instance for arithmetic operations
+  alu_74181_arith alu_74181_arith_inst (.a(a), .b(b), .s(s), .cn(cn), .f(f_arith), .cn4(cn4));
 
   // Outputs
-  assign f = f_logic;
-  assign cn4 = '0;
-  assign equal = '0;
+  assign f = m ? f_logic : f_arith;
+  assign equal = (a === b) ? 1'b1 : 1'b0;
   assign p = '0;
   assign g = '0;
 
