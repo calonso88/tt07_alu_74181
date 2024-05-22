22a71d52f3f11250a4c0639e7687754fb5e20efa
Caio Alonso da Costa <caioalonso@gmail.com>
2024-05-22 19:35:22 +0200

    Update synchronizer.sv

--- src/synchronizer.sv
+++ src/synchronizer.sv
@@ -15,8 +15,8 @@ module synchronizer #(parameter int WIDTH = 4) (rstb, clk, ena, data_in, data_ou
   logic [WIDTH-1:0] data_sync;
   logic [WIDTH-1:0] data_sync2;
 
-  reclocking reclocking_i0 #(.WIDTH(WIDTH)) (.rstb(rstb), .clk(clk), .ena(ena), .data_in(data_in), .data_out(data_sync));
-  reclocking reclocking_i1 #(.WIDTH(WIDTH)) (.rstb(rstb), .clk(clk), .ena(ena), .data_in(data_sync), .data_out(data_sync2));
+  reclocking #(.WIDTH(WIDTH)) reclocking_i0 (.rstb(rstb), .clk(clk), .ena(ena), .data_in(data_in), .data_out(data_sync));
+  reclocking #(.WIDTH(WIDTH)) reclocking_i1 (.rstb(rstb), .clk(clk), .ena(ena), .data_in(data_sync), .data_out(data_sync2));
 
   assign data_out = data_sync2;
 
